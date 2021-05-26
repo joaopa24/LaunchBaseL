0585a5961c61d49027b56d4c53c2950b480f110e
joaopa24 <joaokn129@gmail.com>
2021-05-26 16:17:27 -0300

    refactor routes and add usercontroller

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const routes = express.Router()
+
+const HomeController = require('../app/controllers/HomeController')
+
+const users = require('./users')
+const products = require('./products')
+
+routes.use('/users', users)
+routes.use('/products', products)
+// Home
+routes.get("/", HomeController.index)
+
+// Create
+routes.get("/ads/create",function(req,res){
+    return res.redirect("/products/create")
+})
+
+module.exports = routes
