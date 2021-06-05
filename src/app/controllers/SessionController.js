90196d778805b787d4550a1b9a02c7730c971fa4
joaopa24 <joaokn129@gmail.com>
2021-06-04 22:33:02 -0300

    session login validator

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -2,6 +2,15 @@ module.exports = {
     loginForm(req,res){
        return res.render("session/index")
     },
+    login(req,res){
+       //verificar se o usuario está cadastrado
+       req.session.userId = req.user.id
+       
+       return res.redirect("users")
+       //verificar se o password bate
+
+       //depois colocar o usuário no req.session
+    },
     logout(req,res){
         req.session.destroy()
         return res.redirect("/")
