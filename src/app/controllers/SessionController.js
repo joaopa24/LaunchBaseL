74d39c6620a5897439f2f271b9fdf999f12ed130
joaopa24 <joaokn129@gmail.com>
2021-06-04 22:44:12 -0300

    login

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,12 +1,12 @@
 module.exports = {
     loginForm(req,res){
-       return res.render("session/index")
+       return res.render("session/login")
     },
     login(req,res){
        //verificar se o usuario está cadastrado
        req.session.userId = req.user.id
        
-       return res.redirect("users")
+       return res.redirect("/users")
        //verificar se o password bate
 
        //depois colocar o usuário no req.session
