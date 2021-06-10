f834d249f3618971f39466674d702548b4e03ce9
joaopa24 <joaokn129@gmail.com>
2021-06-10 18:23:35 -0300

    validator email forgot

--- src/app/validators/session.js
+++ src/app/validators/session.js
@@ -22,7 +22,23 @@ async function login(req, res, next) {
 
     next()
 }
+async function forgot(req, res, next) {
+    const { email } = req.body
 
+    try{
+        let user = await Iser.findOne({ where: { email } })
+
+        if (!user) return res.render("session/forgot-password", {
+            user:req.body,
+            error: "Email não cadastrado"
+        })
+
+        next()
+    }catch(err){
+        console.error(err)
+    }
+}
 module.exports = {
-    login
+    login,
+    forgot
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -16,7 +16,7 @@ routes.post('/logout', SessionController.logout)
 // reset password / forgot
 routes.get('/forgot-password', SessionController.forgotForm)
 //routes.get('/password-reset', SessionController.resetForm)
-routes.post('/forgot-password', SessionController.forgot)
+routes.post('/forgot-password', SessionValidator.forgot, SessionController.forgot)
 //routes.post('/password-reset', SessionController.reset)
 
 
