7e62de58b6b0cba76f22f7929ac0a4b4d10f0e6f
joaopa24 <joaokn129@gmail.com>
2021-06-19 23:24:47 -0300

    session token

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,3 +1,4 @@
+const crypt = require('crypto')
 module.exports = {
     loginForm(req,res){
        return res.render("session/login")
@@ -16,10 +17,18 @@ module.exports = {
         return res.render("session/forgot-password")
      },
      forgot(req, res){
+         const user = req.user
+
          // token para esse usúario
-
+         const token = crypto.randomBytes(20).toString("hex")
          // criar uma expiração do token
+         let now = new Date()
+         now = now.setHours(now.getHours() + 1)
 
+         await User.update(user.id, {
+             reset_token:token,
+             reset_token_expires:now
+         })
          // enviar um email com um link de recuperação
 
          // avisar o usuário que enviamos o email
