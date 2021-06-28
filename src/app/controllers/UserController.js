cdfae9ff24ea98048b5d108a484dc37694f4622f
joaopa24 <joaokn129@gmail.com>
2021-06-28 20:13:07 -0300

    user controller delete

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -47,5 +47,20 @@ module.exports = {
                 error:"Algum erro aconteceu"
             })
         }
+    },
+    async delete(req, res){
+         try{
+             await User.delete(1)
+             req.session.destroy()
+
+             return res.render("session/login",{
+                 sucess:"Conta deletada com sucesso!"
+             })
+         }catch(err){
+             console.error(err)
+             return res.render("user/index",{
+                   error:"Erro ao tentar deletar sua conta!"
+             })
+         }
     }
 }
